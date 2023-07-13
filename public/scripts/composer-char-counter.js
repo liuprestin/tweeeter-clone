3f13ee6ab5298cdcb9526185275fff6db180a057
Prestin Liu <liuprestin@proton.me>
2023-07-12 17:18:30 -0700

    add css styling when countdown is low

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     $('#tweet_text').on("input", function() {
         $('#tweet_counter').val(140 - $(this).val().length)
-    })
+        if( $(this).val().length > 100) {
+            // visually show user is beyond threshold
+            $('#tweet_counter').css('color', 'red');
+          } else {
+            $('#tweet_counter').css('color', 'black'); 
+          }
+    })  
   });
